5993fffae356c8f498bd4a2662d2d3ddd450729b
holyhloy <tlegchilin@yandex.ru>
2025-04-08 10:08:16 +0300

    feat: postgres dogs db connected

--- dogs_app/settings.py
+++ dogs_app/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'dogs_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogs',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
