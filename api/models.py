43f76ff1c79e93444d2bb2b280cdac8388badf81
holyhloy <tlegchilin@yandex.ru>
2025-04-08 11:43:33 +0300

    feat: breed model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+
+class Breed(models.Model):
+	"""Class for a Breed model.
+
+	Used to Django ORM was able to bring data to the database
+
+	Has no attributes.
+	"""
+	SIZE_CHOICES = [
+		('tiny', 'Tiny'),
+		('small', 'Small'),
+		('medium', 'Medium'),
+		('large', 'Large')
+	]
+
+	name = models.CharField(max_length=50)
+	size = models.CharField(max_length=50, choices=SIZE_CHOICES)
+	friendliness = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+	trainability = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+	shedding_amount = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+	exercise_needs = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+
+	def __str__(self) -> CharField:
+		"""Tells Django what to return when model object calls
+
+		Returns:
+			A Django CharField 'name' for the model
+		"""
+		return self.name
