00d381ffb11712653815b26172cfcf2dad975f33
holyhloy <tlegchilin@yandex.ru>
2025-04-09 19:02:04 +0300

    refactored DogSerializer, DogViewSet, added list method to viewset

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,9 @@ from .models import Dog, Breed
 
 
 class DogSerializer(serializers.ModelSerializer):
+	breed_average_age = serializers.FloatField(read_only=True)
+	breed_name = serializers.CharField(source='breed.name')
+
 	class Meta:
 		model = Dog
-		fields = '__all__'
-
+		fields = ['id', 'name', 'age', 'gender', 'color', 'favorite_food', 'favorite_toy', 'breed_name', 'breed_average_age']
--- api/views.py
+++ api/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-
+from django.db import models
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from api.models import Dog
 from api.serializers import DogSerializer
 
 
 class DogViewSet(viewsets.ModelViewSet):
-	queryset = Dog.objects.prefetch_related('dogs').all()
-	serializer_class = DogSerializer
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+    def list(self, request, *args, **kwargs):
+        average_age_subquery = Dog.objects.filter(breed=models.OuterRef('breed')).values('breed').annotate(
+            avg_age=models.Avg('age')).values('avg_age')
+        queryset = Dog.objects.annotate(breed_average_age=models.Subquery(average_age_subquery))
+        print(queryset)
+        serializer = DogSerializer(instance=queryset, many=True)
+        return Response(serializer.data)
