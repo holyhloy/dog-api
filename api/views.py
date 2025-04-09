560b869d3ce57f9da9555d2cb842634b9b7856c9
holyhloy <tlegchilin@yandex.ru>
2025-04-09 10:44:09 +0300

    feat: created a dog viewset

--- api/views.py
+++ api/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from api.models import Dog
+from api.serializers import DogSerializer
+
+
+class DogViewSet(viewsets.ModelViewSet):
+	queryset = Dog.objects.prefetch_related('dogs').all()
+	serializer_class = DogSerializer
