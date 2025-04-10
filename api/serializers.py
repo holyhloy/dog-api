2a0f2e3c7da64acdaee29d711297fec04485ef70
holyhloy <tlegchilin@yandex.ru>
2025-04-10 09:47:32 +0300

    feat:update method for DogViewSet added

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from .models import Dog, Breed
 
 class DogSerializer(serializers.ModelSerializer):
 	breed_average_age = serializers.FloatField(read_only=True)
-	breed_name = serializers.CharField(source='breed.name')
+	breed_name = serializers.CharField(source='breed.name', read_only=True)
 	dog_count = serializers.IntegerField(read_only=True)
 
 	class Meta:
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.db import models
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from api.models import Dog, Breed
@@ -20,9 +20,19 @@ class DogViewSet(viewsets.ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         dog_count_subquery = Dog.objects.filter(breed=models.OuterRef('breed')).values('breed').annotate(
             cnt=models.Count('id')).values('cnt')
-        queryset = Dog.objects.filter(id=kwargs.get('pk')).annotate(dog_count=models.Subquery(dog_count_subquery))
-        serializer = DogSerializer(instance=queryset, many=True)
-        return Response(serializer.data)
+        queryset = Dog.objects.annotate(dog_count=models.Subquery(dog_count_subquery))
+        instance = queryset.get(id=kwargs.get('pk'))
+        serializer = DogSerializer(instance=instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = DogSerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data = serializer.data
+            return Response(data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BreedViewSet(viewsets.ModelViewSet):
