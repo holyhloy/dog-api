1d10d2ab2779adbc5e042ece0c429b9cd8b01c3a
holyhloy <tlegchilin@yandex.ru>
2025-04-10 16:05:49 +0300

    feat: update method added to a BreedViewSet

--- api/views.py
+++ api/views.py
@@ -81,3 +81,11 @@ class BreedViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         serializer = self.serializer_class(instance=instance)
         return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance=instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
